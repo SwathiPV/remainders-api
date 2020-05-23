f6c67803a08dec247bbdf45fc44a95d8bb7a480a
SwathiPV <vswathi418@gmail.com>
2020-05-23 18:38:51 +0000

    Using Generic views

--- remainders/urls.py
+++ remainders/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from .views import RemainderAPIView, RemainderDetailsAPIView
+from django.urls import path, re_path
+from .views import RemainderAPIView, RemainderDetailsAPIView, GenericAPIView
 
 urlpatterns = [
     path('remainders/', RemainderAPIView.as_view()),
-    path('remainders/<int:pk>', RemainderDetailsAPIView.as_view())
+    path('remainders/<int:pk>', RemainderDetailsAPIView.as_view()),
+    # re_path(r'generic/remainders/(?P<id>\d+)/$', GenericAPIView.as_view())
+    re_path(r'^generic/remainders/(?:(?P<id>[1-9]+)/)?$', GenericAPIView.as_view())
 ]
