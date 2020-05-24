f639ac44ad809142305abe3a7d18f966b5afe390
SwathiPV <vswathi418@gmail.com>
2020-05-24 14:11:38 +0000

    Using ModelViewSet

--- remainders/urls.py
+++ remainders/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path, re_path
-from .views import RemainderAPIView, RemainderDetailsAPIView, GenericAPIView
+from django.urls import path, re_path, include
+from .views import RemainderAPIView, RemainderDetailsAPIView, GenericAPIView, RemainderViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('remainders', RemainderViewSet, base_name="remainder")
 
 urlpatterns = [
     path('remainders/', RemainderAPIView.as_view()),
     path('remainders/<int:pk>', RemainderDetailsAPIView.as_view()),
     # re_path(r'generic/remainders/(?P<id>\d+)/$', GenericAPIView.as_view())
-    re_path(r'^generic/remainders/(?:(?P<id>[1-9]+)/)?$', GenericAPIView.as_view())
+    re_path(r'^generic/remainders/(?:(?P<id>[1-9]+)/)?$', GenericAPIView.as_view()),
+    path('viewset/', include(router.urls))
 ]
--- remainders/views.py
+++ remainders/views.py
@@ -4,10 +4,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, viewsets
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+class RemainderViewSet(viewsets.ModelViewSet):
+    serializer_class = RemainderSerializer
+    queryset = Remainder.objects.all()
 
 class GenericAPIView(
     generics.GenericAPIView,
